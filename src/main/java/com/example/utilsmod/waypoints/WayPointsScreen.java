613a6c929a7da69965f6c652aa609fbb355cdf9a
psrekha1976@gmail.com <psrekha1976@gmail.com>
2023-02-01 00:15:43 +0530

    test

--- src/main/java/com/example/utilsmod/waypoints/WayPointsScreen.java
+++ src/main/java/com/example/utilsmod/waypoints/WayPointsScreen.java
@@ -1,6 +1,7 @@
 package com.example.utilsmod.waypoints;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
@@ -19,5 +20,6 @@ public class WayPointsScreen extends Screen {
     public void render(PoseStack p_96562_, int p_96563_, int p_96564_, float p_96565_) {
         super.render(p_96562_, p_96563_, p_96564_, p_96565_);
         renderBackground(p_96562_);
+
     }
 }
