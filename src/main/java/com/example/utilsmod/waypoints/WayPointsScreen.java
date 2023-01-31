5b96e9ed25224d47db661eeb03f6368766133ec2
psrekha1976@gmail.com <psrekha1976@gmail.com>
2023-02-01 00:47:02 +0530

    Screen Added

--- src/main/java/com/example/utilsmod/waypoints/WayPointsScreen.java
+++ src/main/java/com/example/utilsmod/waypoints/WayPointsScreen.java
@@ -1,11 +1,14 @@
 package com.example.utilsmod.waypoints;
 
+import com.example.utilsmod.waypoints.buttons.AddButton;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class WayPointsScreen extends Screen {
+
 
     public WayPointsScreen(Component p_96550_) {
         super(p_96550_);
@@ -14,6 +17,7 @@ public class WayPointsScreen extends Screen {
     @Override
     protected void init() {
         super.init();
+       // addRenderableWidget(new AddButton(this.width/2,this.height/2,50,50, Component.literal("AddButton"),WayPointsScreen::OnAddPress, new Button.CreateNarration().createNarrationMessage(Component.literal("Adding"))));
     }
 
     @Override
@@ -21,5 +25,9 @@ public class WayPointsScreen extends Screen {
         super.render(p_96562_, p_96563_, p_96564_, p_96565_);
         renderBackground(p_96562_);
 
+    }
+
+    public static void OnAddPress(Button button){
+        System.out.println("ADD PRESSED");
     }
 }
