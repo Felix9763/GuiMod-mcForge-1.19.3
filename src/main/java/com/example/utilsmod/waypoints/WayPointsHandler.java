74336baab66ad55d240cfb92455c3c05e670f9a6
psrekha1976@gmail.com <psrekha1976@gmail.com>
2023-02-02 22:54:34 +0530

    Almost Done Json INtroduced

--- src/main/java/com/example/utilsmod/waypoints/WayPointsHandler.java
+++ src/main/java/com/example/utilsmod/waypoints/WayPointsHandler.java
@@ -1,12 +1,14 @@
 package com.example.utilsmod.waypoints;
 
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,14 +17,15 @@ import java.util.Scanner;
 public class WayPointsHandler {
     protected static ArrayList<ArrayList<Integer>> coords = new ArrayList<ArrayList<Integer>>();
     protected static List<String> titles = new ArrayList<>();
-    public static HashMap<String,ArrayList<Integer>> map = new HashMap<>();
+    public static HashMap<String,ArrayList<Integer>> Mainmap = new HashMap<>();
+    public static String info = "";
     public static Gson gs = new Gson();
 
     public static void Add(ArrayList<Integer> coord,String title) throws IOException {
         coords.add(coord);
         titles.add(title);
-        map.put(title,coord);
-        AddToFile(map);
+        Mainmap.put(title,coord);
+        AddToFile(Mainmap);
     }
 
     public void Remove(String rTitle){
@@ -40,40 +43,24 @@ public class WayPointsHandler {
 
         File file = new File("C:\\Users\\psrek\\Desktop\\coords.json");
         FileWriter writer = new FileWriter("C:\\Users\\psrek\\Desktop\\coords.json");
-        writer.write(json);
-//        System.out.println('a');
-//        for(int o = 0; o <  coords.size();o++){
-//            for(int i = 0; i <  coords.size();i++){
-//                String scoords = coords.get(o).toString();
-//                writer.write(scoords + titles.get(o) +"\n");
-//                System.out.println(scoords);
-//            }
-//        }
-
-
+        writer.write(info + json);
         writer.close();
     }
 
     public static void ReadFile() throws FileNotFoundException {
-        File file = new File("Desktop/coords.json");
+        File file = new File("C:\\Users\\psrek\\Desktop\\coords.json");
         Scanner sc = new Scanner(file);
-
-        String info = "";
-        while (sc.hasNextLine()){
+        info = "";
+        while (sc.hasNextLine()) {
             info = info.concat(sc.nextLine() + "\n");
+            System.out.println(info);
         }
-    }
+//        TypeToken<HashMap<String,ArrayList<Integer>>> maptype = new TypeToken<HashMap<String,ArrayList<Integer>>>(){};
+//
+//        HashMap<String,ArrayList<Integer>> map = gs.fromJson(info, (Type) maptype);
+//        Mainmap = map;
 
-    public static class CoordsName{
-        String title;
-        public CoordsName(String title){
-            this.title = title;
-        }
-        public static class Coords{
-            ArrayList<Integer> coords = new ArrayList<Integer>();
-            public Coords(ArrayList<Integer> coords){
-                this.coords = coords;
-            }
-        }
+
+
     }
 }
